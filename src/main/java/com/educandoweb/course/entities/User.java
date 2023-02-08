775ce5a74c3d5da2386d85dfa5eca9c11370bd5b
Bruno Sa <brunosantana.engenharia@gmail.com>
2023-02-08 14:02:12 -0300

    Implementado o repositories,service e reources da entidade order

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -5,12 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,12 +21,13 @@ public class User implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)  
-	private long id;
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
+	@JsonIgnore
     @OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
 	
@@ -33,7 +35,7 @@ public class User implements Serializable {
 		
 	}
 
-	public User(long id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
